dfd61f93ac4cba93510156bc16857d8aa0e43066
Matt Bacchi <mbacchi@gmail.com>
2019-08-22 22:59:49 -0600

    Remove NAT gateway

--- main.tf
+++ main.tf
@@ -88,10 +88,8 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
+  enable_nat_gateway = false
   enable_vpn_gateway = false
-  single_nat_gateway = true
-  one_nat_gateway_per_az = false
 
   tags = {
     Automation = "true"
